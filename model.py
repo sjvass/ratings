2c9d62e183aec10a34efffcb278c789468ebf588
Hackbright Student <no-reply@hackbrightacademy.com>
2019-03-23 15:21:17 -0700

    started part2

--- model.py
+++ model.py
@@ -54,6 +54,23 @@ class Rating(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     score = db.Column(db.Integer, nullable=False)
 
+    #Define relationship to user
+    user = db.relationship("User",
+                            backref=db.backref("ratings",
+                                                order_by=rating_id))
+    #Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                                order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id}
+                   movie_id={self.movie_id}
+                   user_id={self.user_id}
+                   score={self.score}>"""
+
 
 ##############################################################################
 # Helper functions
